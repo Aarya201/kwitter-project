3e24cb307772b14cdcd2176198b9e5175b15c9a8
Aarya201 <76944790+Aarya201@users.noreply.github.com>
2021-05-31 13:56:29 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,5 @@
+function madeyeseverusalbus() {
+    var dobby = document.getElementById("riptonkslupinfred").value
+    localStorage.setItem("username", dobby)
+    window.location="kwitter_room.html";
+}
